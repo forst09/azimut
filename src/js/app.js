637131b9b75030e379f69dcc0dcd32ea1b26eb09
Daria <dforst09@gmail.com>
2022-07-01 10:43:45 +0300

    add dropdown

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,17 @@ $(document).on('click', ".tabs__list-item", function () {
     $('.tabs__list-item').removeClass('active');
     $(this).addClass('active');
 });
+
+//раскрытие дропдаунов
+$(document).on('click', ".dropdown", function () {
+    if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        $(this).find('.dropdown__link').removeClass('active');
+        $(this).find('.dropdown__content').removeClass('active');
+    }
+    else {
+        $(this).addClass('active');
+        $(this).find('.dropdown__link').addClass('active');
+        $(this).find('.dropdown__content').addClass('active');
+    }
+});
