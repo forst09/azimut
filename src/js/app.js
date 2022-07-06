6dd368a0d8ed386da86899d12642ce497f8be729
Daria <dforst09@gmail.com>
2022-07-06 18:32:19 +0300

    start switch image

--- src/js/app.js
+++ src/js/app.js
@@ -72,11 +72,16 @@ $(document).ready(function () {
             $(this).removeClass('active');
             $(this).parents('.switch').find('.switch__item-sun').removeClass('active');
             $(this).parents('.switch').find('.switch__item-moon').addClass('active');
+            $(this).parents('.switch').find('.switch__item-moon').addClass('active');
+            $(this).parents('section').find('.switch-image-night').addClass('active');
+            $(this).parents('section').find('.switch-image-day').removeClass('active');
         }
         else {
             $(this).addClass('active');
             $(this).parents('.switch').find('.switch__item-sun').addClass('active');
             $(this).parents('.switch').find('.switch__item-moon').removeClass('active');
+            $(this).parents('section').find('.switch-image-night').removeClass('active');
+            $(this).parents('section').find('.switch-image-day').addClass('active');
         }
     });
 
